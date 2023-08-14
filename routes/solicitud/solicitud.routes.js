feeb6e8936d60002b15b24ab536f7b120f06de4f
Elliot Fernandez <elliot@hispantic.com>
2023-08-14 11:10:33 +0100

    hecho el editor de solicitudes

--- routes/solicitud/solicitud.routes.js
+++ routes/solicitud/solicitud.routes.js
@@ -11,14 +11,15 @@ const uploader = require("../../middlewares/cloudinary.middlewares.js");
 // aqui van las rutas
 // GET "/solicitud/crear" => esto renderiza la vista del formulario de crar solicitudes
 router.get("/crear", isLoggedIn, (req, res, next) => {
-  const usuarioCreador = req.session.user._id
-  console.log(usuarioCreador)
-  res.render("./solicitud/crear-solicitud.hbs", {usuarioCreador});
+  const usuarioCreador = req.session.user._id;
+  console.log(usuarioCreador);
+  res.render("./solicitud/crear-solicitud.hbs", { usuarioCreador });
 });
 
 // POST "/solicitud/crear"
 router.post("/crear", isLoggedIn, async (req, res, next) => {
-  const { titulo, descripcion, categoria, fechaServicio, usuarioCreador } = req.body;
+  const { titulo, descripcion, categoria, fechaServicio, usuarioCreador } =
+    req.body;
   console.log("solicitud creada: ", req.body);
   try {
     const respuesta = await Solicitud.create({
@@ -26,7 +27,7 @@ router.post("/crear", isLoggedIn, async (req, res, next) => {
       descripcion,
       categoria,
       fechaServicio,
-      usuarioCreador
+      usuarioCreador,
     });
     console.log(respuesta);
     res.redirect(`/solicitud/${respuesta._id}/`);
@@ -38,16 +39,15 @@ router.post("/crear", isLoggedIn, async (req, res, next) => {
 // GET para renderizar la vista de la solicitud creada
 router.get("/:solicitudId", isLoggedIn, async (req, res, next) => {
   try {
-    const solicitudObjecto = await Solicitud.findById(req.params.solicitudId)
-    .populate("usuarioCreador")
-    console.log("objeto", solicitudObjecto)
+    const solicitudOb = await Solicitud.findById(
+      req.params.solicitudId
+    ).populate("usuarioCreador");
+    console.log("objeto", solicitudOb);
 
-   
-    res.render("./solicitud/detalles-solicitud.hbs", {solicitudObjecto});
+    res.render("./solicitud/detalles-solicitud.hbs", { solicitudOb });
   } catch (error) {
-    next(error)
+    next(error);
   }
-  
 });
 
 //POST => subir la imagen
@@ -56,7 +56,7 @@ router.post(
   uploader.single("imagenSolicitud"),
   async (req, res, next) => {
     console.log("fichero de la imagen de perfil", req.file);
-    console.log("id de la solicitud", req.body._id)
+    console.log("id de la solicitud", req.body._id);
 
     try {
       await Solicitud.findByIdAndUpdate(req.body._id, {
@@ -68,6 +68,62 @@ router.post(
     }
   }
 );
+
+// GET "/solicitud/:solicitudId/editar" => renderiza el formulario de edición de una solicitud
+router.get("/:solicitudId/editar", isLoggedIn, async (req, res, next) => {
+  const solicitudId = req.params.solicitudId;
+  console.log("solicitud id", solicitudId);
+
+  try {
+    const detallesSolicitud = await Solicitud.findById(solicitudId);
+
+    const categorias = [
+      { name: "bricolaje", isSelected: false },
+      { name: "cuidados", isSelected: false },
+      { name: "mascotas", isSelected: false },
+      { name: "transporte", isSelected: false },
+      { name: "alimentación", isSelected: false },
+      { name: "otros", isSelected: false },
+    ];
+    const cloneCategorias = JSON.parse(JSON.stringify(categorias));
+
+    cloneCategorias.forEach((eachCategoria) => {
+      if (
+        detallesSolicitud.categoria.toString() === eachCategoria.name.toString()
+      ) {
+        eachCategoria.isSelected = true;
+        console.log("la categoria seleccionada es: ", eachCategoria);
+      }
+    });
+
+    console.log("detalles solicitud", detallesSolicitud);
+
+    console.log("categoria: ", cloneCategorias);
+    res.render("./solicitud/editar-solicitud.hbs", {
+      detallesSolicitud: detallesSolicitud,
+      categoriaSeleccionada: cloneCategorias,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST "/solicitud/:solicitudId/editar" => procesa el formulario en la base de datos y actualiza la solicitud
+router.post("/:solicitudId/editar", async (req, res, next) => {
+  const { titulo, descripcion, categoria, fechaServicio } = req.body;
+  const solicitudId = req.params.solicitudId;
+  try {
+    const respuesta = await Solicitud.findByIdAndUpdate(solicitudId, {
+      titulo: titulo,
+      descripcion: descripcion,
+      categoria: categoria,
+      fechaServicio: fechaServicio,
+    });
+    res.redirect(`/solicitud/${solicitudId}`);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // exporta el fichero para poder connectar con él desde cualquier archivo
 module.exports = router;
